1e9a522a97a0225b8257826519335c231f42574a
EllenBeliniDOliveira <117590080+EllenBeliniDOliveira@users.noreply.github.com>
2023-04-05 16:59:16 -0600

    Add files via upload

---
+++ ellen.struct.05-04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    struct dados_do_aluno
+    {
+        char nome [50];
+        float media;
+        int ano;
+    };
+    struct dados_do_aluno aluno;
+    printf("Digite o nome do aluno:");
+    fgets(aluno.nome, 50, stdin);
+    printf("Digite a média escolar:");
+    scanf("%f", &aluno.media);
+    printf("Digite o ano escolar:");
+    scanf("%d", &aluno.ano);
+    
+    printf("Nome:%s", aluno.nome);
+    printf("Média:%f\n", aluno.media);
+    printf("Ano:%d\n", aluno.ano);
+}
